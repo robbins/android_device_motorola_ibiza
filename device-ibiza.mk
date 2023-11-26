a54d2e0bd95144a31194e90a70c31f1e7b2d6bb0
Nathanael Robbins <nejrobbins@gmail.com>
2023-11-26 16:46:56 -0500

    vendor ramdisk temp

--- device-ibiza.mk
+++ device-ibiza.mk
@@ -5,3 +5,15 @@ TARGET_BOARD_PLATFORM := holi
 PRODUCT_SHIPPING_API_LEVEL := 30
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_TARGET_VNDK_VERSION := 30
+
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
+LOCAL_PATH := device/motorola/ibiza
+
+PRODUCT_COPY_FILES := \
+		      $(LOCAL_PATH)/fstab.hardware:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.$(PRODUCT_PLATFORM) \
+		      $(LOCAL_PATH)/fstab.hardware:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.$(PRODUCT_PLATFORM)
+
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+		     vendor_boot
