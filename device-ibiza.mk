b475c4edf293aaca8e0d7f6e4f8dd00c849c9e10
Nathanael Robbins <nejrobbins@gmail.com>
2024-02-16 19:02:58 -0500

    set init loglevel to 7 another way to make sure (also in init.rc)

--- device-ibiza.mk
+++ device-ibiza.mk
@@ -23,3 +23,5 @@ PRODUCT_COPY_FILES += \
         device/motorola/ibiza/rootdir/init.target.rc:$(TARGET_COPY_OUT_ROOT)/init.qcom.rc \
         device/motorola/ibiza/rootdir/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
 	device/motorola/ibiza/rootdir/init.recovery.target.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
+
+PRODUCT_SYSTEM_PROPERTIES += sys.init_log_level=7
