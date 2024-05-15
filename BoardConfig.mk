1b03ac1807506df6a2be6efdf9f1389daf81a4f8
Nathanael Robbins <nejrobbins@gmail.com>
2024-05-14 22:05:40 -0400

    Add vendor manifest
    Device manifest consists of vendor manifest & ODM manifest.
    Vendor manifest: /vendor_a_img/etc/vintf/manifest.xml (included in this
    commit)
    ODM manifest: ./vendor_a_img/etc/vintf/manifest/manifest.xml
    
    Fixes:
    checkvintf I 05-15 01:32:35 171958 171958 HostFileSystem.cpp:43] Fetch 'out/target/product/ibiza/system/vendor/etc/vintf/manifest.xml': NAME_NOT_FOUND
    checkvintf I 05-15 01:32:35 171958 171958 check_vintf.cpp:76] Sysprop ro.boot.product.hardware.sku is missing, default to ''
    checkvintf I 05-15 01:32:35 171958 171958 HostFileSystem.cpp:43] Fetch 'out/target/product/ibiza/system/vendor/manifest.xml': NAME_NOT_FOUND
    checkvintf E 05-15 01:32:35 171958 171958 VintfObject.cpp:68] getDeviceHalManifest: status from fetching VINTF information: -2
    checkvintf E 05-15 01:32:35 171958 171958 VintfObject.cpp:69] getDeviceHalManifest: -2 VINTF parse error: Cannot read out/target/product/ibiza/system/vendor/manifest.xml: No such file or directory
    checkvintf E 05-15 01:32:35 171958 171958 check_vintf.cpp:532] Cannot fetch vendor manifest.
    
    References:
    https://source.android.com/docs/core/architecture/vintf/objects

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := kryo
+
+# Vendor Interface Manifest
+DEVICE_MANIFEST_FILE := device/motorola/ibiza/manifest.xml
