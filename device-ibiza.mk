dc8cf0161f1c9910ff4c1fa7393126183adfe671
Nathanael Robbins <nejrobbins@gmail.com>
2024-05-14 22:38:18 -0400

    Set build variables for A/B
    aosp_ibiza-eng compiles up to the commit before this, but it's not yet
    outputting a flashable build.
    
    Copied directly from AOSP docs. Will need to add more when we add vendor
    boot support, and add some more partitions that are probably needed
    here.
    
    Couldn't find "ro.product.ab_ota_partitions" in firmware images to cross-eference.
    
    References:
    https://source.android.com/docs/core/ota/ab/ab_implement#build-variables

--- device-ibiza.mk
+++ device-ibiza.mk
@@ -1,1 +1,18 @@
 PRODUCT_PLATFORM := holi
+
+# Must define for A/B target
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+  boot \
+  system \
+  vendor
+PRODUCT_PACKAGES += \
+  update_engine \
+  update_verifier
+
+# Strongly recommended for A/B target
+TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+
+# Debug
+PRODUCT_PACKAGES_DEBUG += update_engine_client
