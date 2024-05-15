1d1b01ffb25e071eac8170c939bda414b06ed56e
Nathanael Robbins <nejrobbins@gmail.com>
2024-05-15 00:45:59 +0000

    add a million things idk

--- device-ibiza.mk
+++ device-ibiza.mk
@@ -24,10 +24,13 @@ PRODUCT_COPY_FILES += \
         device/motorola/ibiza/rootdir/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
 	device/motorola/ibiza/rootdir/init.recovery.target.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
-PRODUCT_SYSTEM_PROPERTIES += sys.init_log_level=7
+PRODUCT_COPY_FILES += \
+	device/motorola/ibiza/rootdir/vendor/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.dm_default_key.options_format.version=2
+# PRODUCT_SYSTEM_PROPERTIES += sys.init_log_level=7
+
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.crypto.dm_default_key.options_format.version=2
 
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1 \
@@ -36,12 +39,28 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1-service \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0 \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0.vendor \
+    libstagefrighthw
     #android.hardware.gatekeeper@1.0-impl \
 
 PRODUCT_COPY_FILES += \
     device/motorola/ibiza/rootdir/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so \
-    device/motorola/ibiza/rootdir/vendor/bin/qseecomd:$(TARGET_COPY_OUT_VENDOR)/bin/qseecomd
+    device/motorola/ibiza/rootdir/vendor/bin/qseecomd:$(TARGET_COPY_OUT_VENDOR)/bin/qseecomd \
+    device/motorola/ibiza/rootdir/vendor/lib/libkeymasterdeviceutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libkeymasterdeviceutils.so \
+    device/motorola/ibiza/rootdir/vendor/lib/libkeymasterprovision.so:$(TARGET_COPY_OUT_VENDOR)/lib/libkeymasterprovision.so \
+    device/motorola/ibiza/rootdir/vendor/lib/libkeymasterutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libkeymasterutils.so \
+    device/motorola/ibiza/rootdir/vendor/lib/libqtikeymaster4.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqtikeymaster4.so \
+    device/motorola/ibiza/rootdir/vendor/lib/libqcbor.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqcbor.so \
+    device/motorola/ibiza/rootdir/vendor/lib/libQSEEComAPI.so:$(TARGET_COPY_OUT_VENDOR)/lib/libQSEEComAPI.so \
+    device/motorola/ibiza/rootdir/vendor/lib/vendor.qti.hardware.qteeconnector@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.qteeconnector@1.0.so \
+    device/motorola/ibiza/rootdir/vendor/lib64/libkeymasterdeviceutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterdeviceutils.so \
+    device/motorola/ibiza/rootdir/vendor/lib64/libkeymasterprovision.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterprovision.so \
+    device/motorola/ibiza/rootdir/vendor/lib64/libkeymasterutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterutils.so \
+    device/motorola/ibiza/rootdir/vendor/lib64/libqtikeymaster4.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqtikeymaster4.so \
+    device/motorola/ibiza/rootdir/vendor/lib64/libqcbor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqcbor.so \
+    device/motorola/ibiza/rootdir/vendor/lib64/libQSEEComAPI.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libQSEEComAPI.so \
+    device/motorola/ibiza/rootdir/vendor/lib64/vendor.qti.hardware.qteeconnector@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.qteeconnector@1.0.so
+
 
 #PRODUCT_PACKAGES += \
 #    android.hardware.keymaster@4.1 \
