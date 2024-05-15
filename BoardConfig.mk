c174ff895eff240d9b75d10e74357aa84a58b030
Nathanael Robbins <nejrobbins@gmail.com>
2024-05-15 16:59:28 -0400

    Set fstab_suffix on kernel command line
    Fixes:
    [    2.932417] init: Failed to create FirstStageMount failed to read default fstab for first stage mount
    [    2.941669] init: Failed to mount required partitions early ...
    [    2.947942] init: InitFatalReboot: signal 6
    
    fstab is installed here:
    vendor_ramdisk
    └── first_stage_ramdisk
        └── fstab.holi
    
    The code looks for fstab.<suffix> and we install it as
    fstab.PRODUCT_PLATFORM, where PRODUCT_PLATFORM is 'holi', but [ro.hardware]: [qcom].
    
    References:
    std::string GetFstabPath() {
      for (const char* prop : {"fstab_suffix", "hardware", "hardware.platform"}) {
          std::string suffix;
    
          if (!fs_mgr_get_boot_config(prop, &suffix)) continue;
    
          for (const char* prefix : {// late-boot/post-boot locations
                                     "/odm/etc/fstab.", "/vendor/etc/fstab.",
                                     // early boot locations
                                     "/system/etc/fstab.", "/first_stage_ramdisk/system/etc/fstab.",
                                     "/fstab.", "/first_stage_ramdisk/fstab."}) {

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,7 @@ BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=4e00000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=$(PRODUCT_PLATFORM)
 
 # Dynamic partitions
 BOARD_SUPER_PARTITION_SIZE := 12884901888
