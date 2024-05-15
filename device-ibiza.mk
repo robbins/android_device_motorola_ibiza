2b851cf688d4e64b9cf43628a883a95a91a0bf2f
Nathanael Robbins <nejrobbins@gmail.com>
2024-05-14 23:20:38 -0400

    Implement vendor boot support
    Fstab from ./vendor_boot_img_unpacked/vendor_ramdisk/first_stage_ramdisk/fstab.qcom
    
    References:
    https://source.android.com/docs/core/architecture/partitions/vendor-boot-partitions#build-support

--- device-ibiza.mk
+++ device-ibiza.mk
@@ -5,7 +5,8 @@ AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := \
   boot \
   system \
-  vendor
+  vendor \
+  vendor_boot
 PRODUCT_PACKAGES += \
   update_engine \
   update_verifier
@@ -20,4 +21,5 @@ PRODUCT_PACKAGES_DEBUG += update_engine_client
 # Prebuilt kernel
 TARGET_PREBUILT_KERNEL := device/motorola/ibiza/kernel
 PRODUCT_COPY_FILES += \
-	$(TARGET_PREBUILT_KERNEL):kernel
+	$(TARGET_PREBUILT_KERNEL):kernel \
+	device/motorola/ibiza/fstab.hardware:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.$(PRODUCT_PLATFORM)
