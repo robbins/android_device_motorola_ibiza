2088ac31c526423ca3415f3b6c9b2d12ca78b717
Nathanael Robbins <nejrobbins@gmail.com>
2025-01-19 20:06:08 -0500

    Define board architecture
    Taken from our TWRP tree

---
+++ BoardConfig.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := device/motorola/ibiza
+
+# Identify CPU architecture & ABI
+# Multi-lib, 32-bit and 64-bit
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := kryo
