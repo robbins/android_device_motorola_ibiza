20079bc89516e3a0fc824ab4a0f2fa7208d0b52d
Nathanael Robbins <nejrobbins@gmail.com>
2025-01-21 18:56:25 -0500

    Implement A/B updates
    Reference: https://source.android.com/docs/core/ota/ab/ab_implement

--- device-ibiza.mk
+++ device-ibiza.mk
@@ -1,1 +1,20 @@
 LOCAL_PATH := device/motorola/ibiza
+
+# Use the A/B updater
+AB_OTA_UPDATER := true
+PRODUCT_PACKAGES += \
+  update_engine \
+  update_verifier
+# Debug builds
+PRODUCT_PACKAGES_DEBUG += update_engine_client
+
+# A/B updatable partitions
+AB_OTA_PARTITIONS := \
+  boot \
+  system \
+  vendor \
+  system_ext \
+  product \
+  dtbo \
+  vbmeta \
+  vbmeta_system
