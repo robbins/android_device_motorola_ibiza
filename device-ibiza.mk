d60ae52fb2bf0bd758c6f3e84ce397b5300274af
Nathanael Robbins <31457698+robbins@users.noreply.github.com>
2026-06-20 01:07:23 -0400

    Add prebuilt kernel

--- device-ibiza.mk
+++ device-ibiza.mk
@@ -1,1 +1,8 @@
+LOCAL_PATH := device/motorola/ibiza
+
 PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Prebuilt kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+PRODUCT_COPY_FILES += \
+	$(TARGET_PREBUILT_KERNEL):kernel
